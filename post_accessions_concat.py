13a477c1adf0e1a6bdfefd7c20037b8b642535c0
mpaqu-lib <44206597+mpaqu-lib@users.noreply.github.com>
2020-05-08 10:26:04 -0400

    Update post_accessions_concat.py
    Fixed tabs, exceptions, and deleted unnecessary comments.

--- post_accessions_concat.py
+++ post_accessions_concat.py
@@ -32,9 +32,6 @@ def make_accession(accession, agent_uri, accession_id):
 				 'provenance': f"{provenance_string1} {full_name} {provenance_string2}",
 				 'title': f"{full_name} {title_string}",
 				 'general_note': f"{gen_note_aff}; {gen_note_living}.",
-				 
-#				 'use_restrictions': True, #Needs to be changed to false if they gave us copyright.
-#				 'use_restrictions_note': 'Copyright info.', #Needs to be populated from spreadsheet.
 				 'id_0': str(accession_id['id_0']),
 				 'id_1': str(accession_id['id_1']),
 				 'id_2': str(accession_id['id_2']),
@@ -53,9 +50,9 @@ def make_accession(accession, agent_uri, accession_id):
 	classification['ref'] = affiliation
 
 	try:
- 			acc_dict['classifications'].append(classification)
-	except:
- 			print ('issue with affiliation')
+ 		acc_dict['classifications'].append(classification)
+	except KeyError:
+ 		logging.error ('issue with affiliation', KeyError)
 
 	
 	# Content Description
@@ -100,9 +97,9 @@ def make_accession(accession, agent_uri, accession_id):
 	linked_agent['role'] = 'source'
 	linked_agent['ref'] = agent_uri
 	try:
- 			acc_dict['linked_agents'].append(linked_agent)
-	except:
- 			print ('issue with linked agent')
+ 		acc_dict['linked_agents'].append(linked_agent)
+	except KeyError:
+ 		logging.error ('issue with linked agent', KeyError)
  			
 	# Add donor agent as creator only if they say "yes" they created it	
 	role = str(accession['created'])
@@ -113,101 +110,8 @@ def make_accession(accession, agent_uri, accession_id):
 		linked_agent_creator['ref'] = agent_uri
 		try:
  			acc_dict['linked_agents'].append(linked_agent_creator)
-		except:
- 			print ('issue with creator linked agent')
-					 	
-	#
-	# # Extents
-	# acc_dict['extents'] = []
-	# extent_dict = {}
-	# extent_dict['jsonmodel_type'] = 'extent'
-	# try:
-	# 	extent_dict['extent_type'] = accession['extent_type'].lower().strip() + 's'
-	# except:
-	# 	continue
-	# try:
-	# 	extent_dict['number'] = str(int(accession['extent'])).strip()
-	# except:
-	# 	continue
-	# try:
-	# 	extent_dict['portion'] = accession['portion'].lower().strip()
-	# except:
-	# 	continue
-	#
-	# # Ensures extents array has all the required fields, otherwise an error will be raised when trying to post
-	# if len(extent_dict) > 3:
-	# 	acc_dict['extents'].append(extent_dict)
-	#
-	# Date field
-	# acc_dict['dates'] = []
-	# date_dict = {}
-
-	# if accession['date_type'] != None and accession['begin_date'] != None:
-	# 	if accession['date_type'].lower().strip() == 'inclusive':
-	# 		date_dict['end'] = accession['end_date']
-	# 	if len(str(accession['begin_date'])) > 4 or len(str(accession['begin_date'])) < 4:
-	# 		date_dict['begin'] = '0000'
-	# 	else:
-	# 		date_dict['begin'] = str(int(accession['begin_date']))
-	# 	if accession['certainty'] != None and (accession['certainty'].lower().strip() == 'approximate' or accession['certainty'].lower().strip() == 'inferred' or accession['certainty'].lower().strip() == 'questionable'):
-	# 		date_dict['certainty'] = accession['certainty'].strip()
-	# 	date_dict['date_type'] = accession['date_type'].lower().strip()
-	# 	date_dict['calendar'] = 'gregorian'
-	# 	date_dict['era'] = 'ce'
-	# 	date_dict['jsonmodel_type'] = 'date'
-	# 	date_dict['label'] = 'publication'
-	# 	acc_dict['dates'].append(date_dict)
-	#
-	# # Acquisition Type
-	# try:
-	# 	acc_dict['acquisition_type'] = accession['\ufeffacquisition_type'].lower().strip()
-	# except:
-	# 	continue
-	#
-	# # Linked agents
-	# acc_dict['linked_agents'] = []
-	#
-	# if accession['agent_type1'] != None:
-	# 	for key in RELATOR_DICT.keys():
-	# 		if key == accession['agent_type1'].lower().strip():
-	# 			relator1 = RELATOR_DICT[key]
-	# 		else:
-	# 			relator1 = ""
-	#
-	# if accession['agent_type2'] != None:
-	# 	for key in RELATOR_DICT.keys():
-	# 		if key == accession['agent_type2'].lower().strip():
-	# 			relator2 = RELATOR_DICT[key]
-	# 		else:
-	# 			relator2 = ""
-	#
-	# agent1 = {}
-	# agent1['terms'] = []
-	# try:
-	# 	if accession['agent_uri1'][0] == '/':
-	# 		agent1['ref'] = accession['agent_uri1'].strip()
-	# 		if relator1 != "":
-	# 			agent1['relator'] = relator1
-	# 		if accession['linked_agent_role1'].strip() != None:
-	# 			agent1['role'] = accession['linked_agent_role1'].lower().strip()
-	#
-	# 		acc_dict['linked_agents'].append(agent1)
-	# except:
-	# 	continue
-	#
-	# agent2 = {}
-	# agent2['terms'] = []
-	# try:
-	# 	if accession['agent_uri2'][0] == '/':
-	# 		agent2['ref'] = accession['agent_uri2'].strip()
-	# 		if relator2 != "":
-	# 			agent2['relator'] = relator1
-	# 		if accession['linked_agent_role2'].strip() != None:
-	# 			agent2['role'] = accession['linked_agent_role2'].lower().strip()
-	#
-	# 		acc_dict['linked_agents'].append(agent2)
-	# except:
-	# 	continue
+		except KeyError:
+ 			logging.error ('issue with creator linked agent', KeyError)
 
 	return acc_dict
 

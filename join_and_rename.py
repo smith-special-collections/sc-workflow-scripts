992238274c4f308cac0b53e95fbc2414c1a5c9e7
mpaqu-lib <44206597+mpaqu-lib@users.noreply.github.com>
2020-05-11 10:30:30 -0400

    Fixed filename for gforma

--- join_and_rename.py
+++ join_and_rename.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 gformb = pd.read_csv("(responses) COVID-19 Chronicles electronic file upload form - Form Responses 1.csv")
-gforma = pd.read_csv("Covid-19 Chronicles contributor form (Responses) - Form Responses 1-2.csv")
+gforma = pd.read_csv("Covid-19 Chronicles contributor form (Responses) - Form Responses 1.csv")
 
 merged = pd.merge(gformb, gforma, how='left', on=['Email Address'])
 merged.to_csv("merged_responses.csv", index=False)

1ad5963b5498d0f1ad0397a7f7734a4f223a8e24
mpaqu-lib <44206597+mpaqu-lib@users.noreply.github.com>
2024-10-04 14:16:02 -0400

    Update aos_with_context.sql
    Added all enumeration value translations.

--- sql_queries/aos_with_context.sql
+++ sql_queries/aos_with_context.sql
@@ -1,25 +1,25 @@
 SELECT
 archival_object.title,
-archival_object.level_id,
+enumeration_value.`value` AS `level of description`,
+enumeration_value_alias1.`value` AS `date type`,
 date.`begin`,
 date.`end`,
 date.expression,
-date.certainty_id,
-date.date_type_id,
+enumeration_value_alias2.`value` AS `date certainty`,
 archival_object_alias1.title AS Parent,
 archival_object_alias2.title AS Grandparent,
 archival_object_alias3.title AS `Great-Grandparent`,
-extent.portion_id,
+enumeration_value_alias3.`value` AS `extent portion`,
 extent.number,
-extent.extent_type_id,
+enumeration_value_alias5.`value` AS `extent type`,
 extent.container_summary,
 extent.physical_details,
 extent.dimensions,
-top_container.type_id,
+enumeration_value_alias4.`value` AS `top container type`,
 top_container.indicator,
-sub_container.type_2_id,
+enumeration_value_alias7.`value` AS 'child type',
 sub_container.indicator_2,
-sub_container.type_3_id,
+enumeration_value_alias6.`value` AS 'grandchild type',
 sub_container.indicator_3
 FROM
 date
@@ -40,6 +40,22 @@ ON sub_container.instance_id = instance.id
 LEFT JOIN top_container_link_rlshp
 ON top_container_link_rlshp.sub_container_id = sub_container.id 
 LEFT JOIN top_container
-ON top_container_link_rlshp.top_container_id = top_container.id
+ON top_container_link_rlshp.top_container_id = top_container.id 
+LEFT JOIN enumeration_value
+ON enumeration_value.id = archival_object.level_id 
+LEFT JOIN enumeration_value AS enumeration_value_alias1
+ON date.date_type_id = enumeration_value_alias1.id 
+LEFT JOIN enumeration_value AS enumeration_value_alias2
+ON date.certainty_id = enumeration_value_alias2.id 
+LEFT JOIN enumeration_value AS enumeration_value_alias3
+ON extent.portion_id = enumeration_value_alias3.id 
+LEFT JOIN enumeration_value AS enumeration_value_alias5
+ON extent.extent_type_id = enumeration_value_alias5.id 
+LEFT JOIN enumeration_value AS enumeration_value_alias4
+ON top_container.type_id = enumeration_value_alias4.id 
+LEFT JOIN enumeration_value AS enumeration_value_alias7
+ON sub_container.type_2_id = enumeration_value_alias7.id 
+LEFT JOIN enumeration_value AS enumeration_value_alias6
+ON sub_container.type_3_id = enumeration_value_alias6.id
 WHERE
-smith.archival_object.root_record_id = '[INSERT NUMBER FROM COLLECTION URI HERE]'
+smith.archival_object.root_record_id = '[INSERT COLLECTION NUMBER FROM URI HERE]'
